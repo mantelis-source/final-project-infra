58c16bc47d7c4dd5f030935f716ec8747f1c78c1
Mantas Andriekus <mantas.andriekus@gmail.com>
2024-01-30 22:32:09 +0200

    Added security group to stanging environment

--- environments/staging/variables.tf
+++ environments/staging/variables.tf
@@ -1,8 +1,13 @@
 variable "vpc_name" {
   type = string
-  default = "vpc-from-module"
+  default = "Staging environment"
 }
 variable "vpc_cidr_range" {
   type = string
   default = "15.85.0.0/16"
+}
+# Local variables
+locals {
+  vpc_id = module.staging_vpc.vpc_idx
+  db_subnets_group = module.staging_vpc.db_subnet_name
 }
--- environments/staging/vpc.tf
+++ environments/staging/vpc.tf
@@ -2,7 +2,7 @@ module "staging_vpc" {
   source = "../../modules/my_vpc"
 
   vpc_cidr_range = var.vpc_cidr_range
-  vpc_name = "Staging environment"
+  vpc_name = var.vpc_name
   azs = ["eu-central-1a", "eu-central-1b"]
   public_subnets = ["15.85.1.0/24"]
   private_subnets = ["15.85.10.0/24", "15.85.11.0/24"]
@@ -10,9 +10,3 @@ module "staging_vpc" {
   dns_hostnames = true
   map_public_ip = true
 }
-  output "vpc_id_from" {
-    value = module.staging_vpc.vpc_idx
-  }
-    output "subnetgroup" {
-    value = module.staging_vpc.db_subnet_name
-  }
