23169a5ae8bcfbcab01db384b551d347e5c8bb17
Mantas Andriekus <mantas.andriekus@gmail.com>
2024-03-03 18:15:31 +0200

    Initial workflow for infrastructure

--- environments/staging/logs/main.tf
+++ environments/staging/logs/main.tf
@@ -1,6 +1,7 @@
 resource "aws_cloudwatch_log_group" "final-project" {
   name = var.log_group_name
   skip_destroy = false
+  retention_in_days = 3
 }
 resource "aws_cloudwatch_log_stream" "final-project" {
   name = var.log_group_name
