5dc1c7dc8669cdc5c6de2c605e286c006ecc6ac2
Mantas Andriekus <mantas.andriekus@gmail.com>
2024-03-04 16:28:02 +0200

    Update infra

--- environments/staging/1_main.tf
+++ environments/staging/1_main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  #  shared_config_files      = ["/home/mantas/.aws/config"]
-  #  shared_credentials_files = ["/home/mantas/.aws/credentials"]
-  #  profile                  = "default"
+#  shared_config_files      = ["/home/mantas/.aws/config"]
+#  shared_credentials_files = ["/home/mantas/.aws/credentials"]
+#  profile                  = "default"
   region     = "eu-central-1"
 }
 module "vpc_subnets" {
